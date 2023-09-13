b36565093c62df7cfc4195aae118aa1c478609ff
Ikechukwu-attah <attahikechukwu@gmail.com>
2023-09-13 11:45:48 -0400

    default hello world changed to my name and ID

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const app = require('./app')
+/********************************************************************************** 
+ * WEB322 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *  Name: _Ikechukwu Anthony Attah_______ Student ID: __123456789_ Date: __10/09/2023__
+ *  Online (Cyclic) URL: https://precious-pink-fossa.cyclic.app/
+ * *********************************************************************************
+    
+    */
 
-const port = process.env.PORT || 3000
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
